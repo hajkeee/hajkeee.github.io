ec7a1d2d7292564a3489ea75f53b15c90a30a07c
hajkeee <hajkeee1@gmail.com>
2024-08-07 11:50:57 +0200

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,131 +1,22 @@
-function main() {
-  const campaignsIterator = AdsApp.campaigns()
-    .withCondition("Status = 'ENABLED'")
-    .withCondition("AdvertisingChannelType = 'UNIVERSAL_APP'")
-    .get();
-
-  while (campaignsIterator.hasNext()) {
-    const campaign = campaignsIterator.next();
-    const adGroupsIterator = campaign.adGroups()
-      .withCondition("Status = 'ENABLED'")
-      .get();
-
-    while (adGroupsIterator.hasNext()) {
-      const adGroup = adGroupsIterator.next();
-      const adGroupId = adGroup.getId();
-      const adGroupName = adGroup.getName();
-
-      const stats = adGroup.getStatsFor("LAST_48_HOURS");
-
-      if (stats.getImpressions() === 0) {
-        // Copy and create the new ad group
-        const newAdGroupBuilder = campaign.newAdGroupBuilder()
-          .withName(adGroupName + " - Copy")
-          .withStatus("ENABLED")
-          .withAdGroupType("UNIVERSAL_APP_ADS");
-
-        const newAdGroup = newAdGroupBuilder.build().getResult();
-
-        // Copy settings
-        newAdGroup.bidding().setCpc(adGroup.bidding().getCpc());
-
-        // Copy keywords if applicable
-        const keywordsIterator = adGroup.keywords().get();
-        while (keywordsIterator.hasNext()) {
-          const keyword = keywordsIterator.next();
-          newAdGroup.newKeywordBuilder()
-            .withText(keyword.getText())
-            .withCpc(keyword.bidding().getCpc())
-            .build();
-        }
-
-        // Copy ads
-        const adsIterator = adGroup.ads().get();
-        while (adsIterator.hasNext()) {
-          const ad = adsIterator.next();
-          const adType = ad.getType();
-          const adBuilder = newAdGroup.newAdBuilder();
-
-          switch (adType) {
-            case "TEXT_AD":
-              adBuilder
-                .withHeadlinePart1(ad.getHeadlinePart1())
-                .withHeadlinePart2(ad.getHeadlinePart2())
-                .withDescription(ad.getDescription())
-                .withFinalUrl(ad.urls().getFinalUrl())
-                .build();
-              break;
-            case "EXPANDED_TEXT_AD":
-              adBuilder
-                .withHeadlinePart1(ad.getHeadlinePart1())
-                .withHeadlinePart2(ad.getHeadlinePart2())
-                .withHeadlinePart3(ad.getHeadlinePart3())
-                .withDescription1(ad.getDescription1())
-                .withDescription2(ad.getDescription2())
-                .withFinalUrl(ad.urls().getFinalUrl())
-                .build();
-              break;
-            case "UNIVERSAL_APP_AD":
-              adBuilder
-                .withHeadline(ad.getHeadline())
-                .withDescription(ad.getDescription())
-                .withFinalUrl(ad.urls().getFinalUrl())
-                .build();
-              break;
-            case "RESPONSIVE_DISPLAY_AD":
-              adBuilder
-                .withShortHeadline(ad.getShortHeadline())
-                .withLongHeadline(ad.getLongHeadline())
-                .withDescription(ad.getDescription())
-                .withBusinessName(ad.getBusinessName())
-                .withFinalUrl(ad.urls().getFinalUrl())
-                .build();
-              break;
-            case "VIDEO_AD":
-              adBuilder
-                .withVideoId(ad.getVideoId())
-                .withFinalUrl(ad.urls().getFinalUrl())
-                .build();
-              break;
-            case "RESPONSIVE_SEARCH_AD":
-              adBuilder
-                .withHeadlines(ad.getHeadlines())
-                .withDescriptions(ad.getDescriptions())
-                .withFinalUrl(ad.urls().getFinalUrl())
-                .build();
-              break;
-            // Add more cases for different ad types if necessary
-            default:
-              Logger.log('Ad type not handled: ' + adType);
-              break;
-          }
-        }
-
-        // Pause the original ad group
-        adGroup.pause();
-      }
-    }
-  }
-}
-
-
-
-function () {
-  try {
-    var pagePath = {{Page Path}};
-    
-    if (!pagePath.includes("/s/")) {
-      return pagePath;
+(function() {
+    // Функція для створення кукі
+    function setCookie(name, value, minutes) {
+        var date = new Date();
+        date.setTime(date.getTime() + (minutes * 60 * 1000));
+        var expires = "expires=" + date.toUTCString();
+        document.cookie = name + "=" + value + ";" + expires + ";path=/";
     }
 
-    pagePath = document.location.hash;
-    pagePath = pagePath.split('/');
-    pagePath.splice(0, 2);
-    pagePath = pagePath.join('/');
-    
-  } catch (e) {
-    console.log('CJ - Page Path Store Pages - ' + e.message);
-  }
+    // Перевірка наявності змінної dataLayer
+    if (window.dataLayer) {
+        // Витягуємо значення змінної з dataLayer
+        var pageLabel = window.dataLayer.find(function(item) {
+            return item.page_label !== undefined;
+        })?.page_label;
 
-  return pagePath;
-}
+        if (pageLabel) {
+            // Створюємо або оновлюємо кукі на 30 хвилин
+            setCookie('page_label', pageLabel, 30);
+        }
+    }
+})();
